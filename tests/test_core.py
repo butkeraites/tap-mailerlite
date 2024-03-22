8efbe85a5682fe6ab5ce7803bd1dcc1951ab00ea
Renan Brito Cano Butkeraites <rbritobut@gmail.com>
2024-03-22 11:47:45 -0300

    Initial configurations to run standard tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,10 +6,15 @@ from singer_sdk.testing import get_tap_test_class
 
 from tap_mailerlite.tap import TapMailerLite
 
-SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    # TODO: Initialize minimal tap config
-}
+import json
+
+with open(".secrets/config.json") as json_file:
+    data = json.load(json_file)
+    SAMPLE_CONFIG = {
+        "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
+        "auth_token": data["TAP_MAILERLITE_AUTH_TOKEN"],
+        "project_ids": [data["PROJECT_ID"]],
+    }
 
 
 # Run standard built-in tap tests from the SDK:
